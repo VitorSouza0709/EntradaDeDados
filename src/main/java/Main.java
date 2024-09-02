97ad5ca54b11c1d20ffaaa0cae81466e76f51522
Vitor <169068391+VitorSouza0709@users.noreply.github.com>
2024-09-02 12:15:52 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+
+//    System.out.println("Entre com um número racional:");
+//    double racional = sc.nextDouble();
+//    System.out.println("O número digitado foi:");
+ //   System.out.println(racional);
+    
+ //   sc.close();
+
+System.out.println("Entre com o nome de seu futuro filho(a):");
+    String racional = sc.nextDouble();
+    System.out.println("O número digitado foi:");
+    System.out.println(racional);
+    
+    sc.close();
+  }
+
+  
+}
